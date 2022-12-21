5c7b329a92054b887afd627270adc8d987324a38
trucnguyentps <trucnguyen@tpssoft.com>
2022-12-21 15:16:05 +0700

    minor update

--- hub/resource_group.tf
+++
@@ -1,9 +0,0 @@
-###
-# Azure resource group
-###
-
-resource "azurerm_resource_group" "rg_vnet_hub" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-  tags     = var.tags
-}
